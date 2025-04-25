30857f882f5987cebca8fde7b1db21abf1f04b26
mdislamulhaque <islamulbd24@gmail.com>
2025-04-25 18:38:15 +0600

    fix responsive issue

--- src/js/Navbar.js
+++ src/js/Navbar.js
@@ -2,12 +2,22 @@ const menuToggle = document.getElementById("menuToggle");
 const hamburgerIcon = document.getElementById("hamburgerIcon");
 const closeIcon = document.getElementById("closeIcon");
 const mobileMenu = document.getElementById("mobileMenu");
+const navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    navbar.classList.add("bg-white", "shadow-md"); // or any bg color you prefer
+  } else {
+    navbar.classList.remove("bg-white", "shadow-md");
+  }
+});
 
 
 menuToggle.addEventListener("click", () => {
   mobileMenu.classList.toggle("hidden");
   hamburgerIcon.classList.toggle("hidden");
   closeIcon.classList.toggle("hidden");
+  navbar.classList.toggle("bg-white")
 });
 
 
@@ -27,3 +37,5 @@ menuToggle.addEventListener("click", () => {
     e.preventDefault();
     menuMobile.classList.toggle("hidden");
   });
+
+   
